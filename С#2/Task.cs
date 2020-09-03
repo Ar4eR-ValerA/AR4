d5a9d147dbc5aceac5d2a78e6d872e39f5436900
Valery Shevchenko <60439555+Ar4eR-ValerA@users.noreply.github.com>
2020-09-03 22:10:54 +0500

    Add files via upload

---
+++ С#2/Task.cs
@@ -0,0 +1,42 @@
+﻿namespace ConsoleApplication1
+{
+    public class Task
+    {
+        public string id, name, info;
+        public bool check = false;
+
+        public string Convert()
+        {
+            string st = "Невыполнена ";
+            if (check)
+            {
+                st = "Выполнена ";
+            }
+            st += id + " " + name + " " + info;
+            return st;
+        }
+
+        public Task(string activeId, string activeName, string activeInfo)
+        {
+            id = activeId;
+            name = activeName;
+            info = activeInfo;
+        }
+        
+        public Task(string activeId, string activeName, string activeInfo, bool activeCheck)
+        {
+            id = activeId;
+            name = activeName;
+            info = activeInfo;
+            check = activeCheck;
+        }
+        
+        public Task(Task activeTask, bool activeCheck)
+        {
+            id = activeTask.id;
+            name = activeTask.name;
+            info = activeTask.info;
+            check = activeCheck;
+        }
+    }
+}
---
+++ С#2/Tasks.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ConsoleApplication1
+{
+    public class Tasks
+    {
+        public List<Task> arr;
+
+        public Tasks()
+        {
+            arr = new List<Task>();
+        }
+
+        public void Add(Task a)
+        {
+            arr.Add(a);
+        }
+
+        public void All()
+        {
+            foreach (Task a in arr)
+            {
+                Console.Write("   " + a.id + " " + a.name + " " + a.info + "\n");
+            }
+        }
+
+
+        public void Save(string nameOfWriteFile, bool check)
+        {
+            using (StreamWriter sw = new StreamWriter($@"{nameOfWriteFile}", check, System.Text.Encoding.Default))
+            {
+                foreach (Task temp in arr)
+                {
+                    string activeTask = temp.Convert();
+                    sw.WriteLine(activeTask);
+                }
+            }
+        }
+
+        public void Load(string nameOfReadFile, bool check)
+        {
+            using (StreamReader sr = new StreamReader($@"{nameOfReadFile}"))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] taskFromFile = line.Split(' ');
+                    if ((taskFromFile[0] == "True") != check)
+                        continue;
+                    string newId = taskFromFile[1], newNameOfTask = taskFromFile[2], newInfo = "";
+                    for (int i = 3; i < taskFromFile.Length; i++)
+                    {
+                            newInfo = newInfo + " " + taskFromFile[i]; 
+                    }
+                    arr.Add(new Task(newId, newNameOfTask, newInfo, check));
+                }
+            }
+        }
+    }
+}
